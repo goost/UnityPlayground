33fde9474c84b189fb823bc1b0aac97f0495e280
Gleb Ostrowski <glebos@web.de>
2015-06-24 07:58:54 +0200

    Added RollABallAgain

---
+++ RollABallAgain/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject _player;
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () {
+		_offset = transform.position - _player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = _player.transform.position + _offset;
+	}
+}
