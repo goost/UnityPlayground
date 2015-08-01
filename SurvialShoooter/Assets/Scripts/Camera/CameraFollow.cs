f47f32d8a40740c190d09cc1fc401ff62577138d
goost <glebos@web.de>
2015-08-01 09:26:26 +0200

    First steps on SurvivalShooter Tut

---
+++ SurvialShoooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothing = 5f;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - _target.position;
+    }
+
+    void FixedUpdate()
+    {
+        var targetCam = _target.position + _offset;
+        transform.position = Vector3.Slerp(transform.position, targetCam,_smoothing*Time.deltaTime); 
+    }
+
+
+}
---
+++ SurvialShoooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 6f;
+    Vector3 _movement;
+    Animator _anim;
+    Rigidbody _rb;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        _anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        _movement.Set(h, 0, v);
+        _movement = _movement.normalized*_speed*Time.deltaTime;
+       _rb.MovePosition(transform.position + _movement); 
+    }
+
+    void Turning()
+    {
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if (!Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) return;
+        var playerToMouse = floorHit.point - transform.position;
+        playerToMouse.y = 0;
+        var newRotation = Quaternion.LookRotation(playerToMouse);
+        _rb.MoveRotation(newRotation);
+    }
+
+    void Animating(float h, float v)
+    {
+        var walking = h != 0f || v != 0f;
+        _anim.SetBool("IsWalking", walking);
+    }
+}
